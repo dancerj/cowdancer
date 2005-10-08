7d3a414bf66ec0d8e227b9df5931a42dfabebf44
dancer <dancer>
2005-10-08 05:41:40 +0000

    add chmod/chown support for testsuite.

---
+++ tests/012_test_chown.c
@@ -0,0 +1,32 @@
+/*BINFMTC:
+chown test */
+
+/* for fchown */
+#define _BSD_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv)
+{
+  uid_t uid=getuid();
+  gid_t gid;
+  int fid;
+  
+  /* test that the four args work. */
+  if(argc < 5)
+    exit(1);
+  
+  gid=atoi(argv[1]);		/* get the gid to change to */
+  
+  chown(argv[2], uid, gid);
+  fid=open(argv[3], O_RDONLY);
+  fchown(fid, uid, gid);
+  close(fid);
+  lchown(argv[4], uid, gid);
+
+  return 0;
+}
---
+++ tests/013_test_chmod.c
@@ -0,0 +1,26 @@
+/*BINFMTC:
+ chmod test */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+/* chmod to 0400 */
+int main(int argc, char** argv)
+{
+  int fid;
+  
+  /* test that the two args work. */
+  if(argc < 3)
+    exit(1);
+  
+  chmod(argv[1], 0400);
+  fid=open(argv[2], O_RDONLY);
+  if (fchmod(fid, 0400))
+    perror("fchmod");
+  close(fid);
+	
+  return 0;
+}
