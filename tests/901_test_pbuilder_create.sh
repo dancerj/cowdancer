ccd186ffe43a72b995aafda36df249f9df3da1da
dancer <dancer>
2006-06-11 04:50:05 +0000

    add testsuites for cowbuilder --create

--- tests/901_test_pbuilder_create.sh
+++ tests/901_test_pbuilder_create.sh
@@ -7,11 +7,11 @@ for DEBOOTSTRAP in debootstrap cdebootstrap; do
     for DISTRIBUTION in etch sid; do
 	sudo rm -rf /var/cache/pbuilder/base-test.cow
 	if [ $DEBOOTSTRAP = cdebootstrap ]; then
-	    DEBUG=--debug
+	    DEBUG="--debootstrapopts --debug"
 	else
 	    DEBUG=
 	fi
-	if sudo cowbuilder --create --debootstrapopts $DEBUG --distribution $DISTRIBUTION --debootstrap $DEBOOTSTRAP --basepath /var/cache/pbuilder/base-test.cow; then
+	if sudo cowbuilder --create $DEBUG --distribution $DISTRIBUTION --debootstrap $DEBOOTSTRAP --basepath /var/cache/pbuilder/base-test.cow; then
 	    echo "[OK]   $DEBOOTSTRAP $DISTRIBUTION" >> tests/log/901_test_pbuilder_create_distributions.log
 	else
 	    echo "[FAIL] $DEBOOTSTRAP $DISTRIBUTION" >> tests/log/901_test_pbuilder_create_distributions.log
