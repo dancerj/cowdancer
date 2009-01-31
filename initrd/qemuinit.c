0609d021871699804371ab231d8d919d15cefbe3
Junichi Uekawa <dancer@netfort.gr.jp>
2009-01-31 16:33:11 +0900

    more tweaks to get initrd working, and make check going further.
    I'm just seeing something booting, and that's a progress.

--- initrd/qemuinit.c
+++ initrd/qemuinit.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/mount.h>
 
@@ -18,17 +20,38 @@
 int main(int argc, char** argv)
 {
   printf(" -> qemu-pbuilder first-stage(initrd version)\n");
-  mount("/proc", "/proc", 
-	"proc", MS_MGC_VAL, NULL);
-  forkexeclp("insmod", "insmod", "lib/modules/ext3.ko", NULL);
-  forkexeclp("cp", "cp", "/proc/mounts", "/etc/mtab", NULL);
+  umask(S_IWGRP | S_IWOTH);
+
+  chdir("/");
   setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", 1);
   setenv("PBUILDER_MOUNTPOINT", PBUILDER_MOUNTPOINT, 1);
+
+  mkdir("/proc", 0777);
+  mkdir("/dev", 0777);
+  mkdir("/dev/pts", 0777);
+  if (mount("/proc", "/proc", 
+	    "proc", MS_MGC_VAL, NULL) == -1) 
+    {
+      perror("mount proc");
+    }
+
+  if (mount("/dev/pts", "/dev/pts", 
+	    "devpts", MS_MGC_VAL, NULL) == -1) 
+    {
+      perror("mount devpts");
+    }
+
+  forkexeclp("/bin/insmod", "/bin/insmod", "lib/modules/ext3.ko", NULL);
+  forkexeclp("/bin/cp", "/bin/cp", "/proc/mounts", "/etc/mtab", NULL);
   mount("/dev/rootdevice", ROOT_MOUNTPOINT, "ext3", 
 	MS_MGC_VAL, NULL);
   mount("/dev/commanddevice", 
 	ROOT_MOUNTPOINT PBUILDER_MOUNTPOINT, 
 	"ext3", MS_MGC_VAL, NULL);
+  
+  /* invoke debug shell for the fun of it -- why doesn't it accept any input?? */
+  forkexeclp("/bin/sh", "/bin/sh");
+  
   chroot("root/");
   chdir("/");
   forkexeclp("bin/sh", "bin/sh", PBUILDER_MOUNTPOINT "/pbuilder-run");
