41b163e4e1f2b76d62ea6d1cfdd533264b52a25c
Junichi Uekawa <dancer@netfort.gr.jp>
2009-02-08 05:47:13 +0900

    update copyright year to 2009.

--- forkexec.c
+++ forkexec.c
@@ -1,6 +1,6 @@
 /*BINFMTC:
  *  fork/exec helper for pbuilder.
- *  Copyright (C) 2007-2008 Junichi Uekawa
+ *  Copyright (C) 2007-2009 Junichi Uekawa
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
