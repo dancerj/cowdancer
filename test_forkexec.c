74d3d3ae3422a714b5e3800ad122b2266480597d
Junichi Uekawa <dancer@netfort.gr.jp>
2009-02-08 01:14:33 +0900

    add spacing after command in forkexec.c debug output.

--- test_forkexec.c
+++ test_forkexec.c
@@ -19,9 +19,16 @@ int main()
       "/bin/false", 
       NULL
     };
+  char *const echovp[] = 
+    {
+      "/bin/echo", 
+      "Hello world",
+      NULL
+    };
   
   assert(forkexecvp(successvp) == 0);
   assert(forkexecvp(failurevp) == 1);
+  assert(forkexecvp(echovp) == 0);
 
   assert(forkexeclp("/bin/true",
 		    "/bin/true",
@@ -29,6 +36,10 @@ int main()
   assert(forkexeclp("/bin/false",
 		    "/bin/false",
 		    NULL) == 1);
+  assert(forkexeclp("/bin/echo",
+		    "/bin/echo",
+		    "Hello World",
+		    NULL) == 0);
   
   return 0;
 }
