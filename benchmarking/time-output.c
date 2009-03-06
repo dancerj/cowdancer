7f44ffe11d348968f1006806df26e67c6ce24590
Junichi Uekawa <dancer@netfort.gr.jp>
2009-03-06 12:01:38 +0900

    file-teeing feature added.

--- benchmarking/time-output.c
+++ benchmarking/time-output.c
@@ -3,7 +3,7 @@
   Pipe through this program to get time information on each line of text.
 
     Use it like:
-    sudo cowbuilder --update  | ./time-output.c
+    sudo cowbuilder --update  | ./time-output.c [optional: file-to-tee]
 
  */
 #include <stdio.h>
@@ -25,19 +25,34 @@ void fix_terminal()
     }
 }
 
-int main()
+int main(int argc, char** argv)
 {
   time_t start = time(NULL);
   char b[4096];
+  FILE* f = NULL;  
 
+  if (argc == 2) 
+    {
+      printf("Output tee to extra file %s\n", argv[1]);
+      f = fopen(argv[1], "w");
+    }
+  
   while(fgets(b,sizeof(b),stdin)!=0)
     {
       printf("[%i] %s", 
 	     (int)difftime(time(NULL), start), 
 	     b);
       fflush(stdout);
+      if (f) 
+	{
+	  fprintf(f, "[%i] %s", 
+		  (int)difftime(time(NULL), start), 
+		  b);
+	  fflush(f);
+	}
     }
 
+  if (f) fclose(f);
   fix_terminal();
   return 0;
 }
